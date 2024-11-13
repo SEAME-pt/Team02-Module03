6d4ee95761669bdeb08d06081c20e2e8c995d2f8
ruipires999 <ruipedro.pires999@gmail.com>
2024-11-13 15:14:53 +0000

    Remove build artifacts and temporary files from repository

--- ex00/build/.qt/QtDeployTargets-RelWithDebInfo.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ex00_FILE C:/Users/Rui Pires/Team02-WarmUp/Module03/ex00/build/RelWithDebInfo/ex00.exe)
-set(__QT_DEPLOY_TARGET_ex00_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ex00_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
